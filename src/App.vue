daaf29ba0e7fa6e9c6ce98bda4582c5de3c885f4
Jonathan Lee <jonathancklee@gmail.com>
2024-09-15 23:30:47 +1000

    Added basic project template

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-slate-500 w-full py-6">
+        <AppHeader />
+        <main class="w-full bg-black"></main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from "./components/AppHeader.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h1 class="relative text-4xl font-bold text-center">
+            <div class="absolute inset-0 z-20">
+                <span class="text-white">Shadow</span> <span class="text-black">Generator</span>
+            </div>
+            <div class="relative translate-x-2 translate-y-2 opacity-40  z-10">
+                <span class="text-black">Shadow</span> <span class="text-white">Generator</span>
+            </div>
+        </h1>
+    </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
