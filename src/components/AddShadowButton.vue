436469a002f43d1f590cdfaecf1f0b3060a765c6
Jonathan Lee <jonathancklee@gmail.com>
2024-10-09 22:53:56 +1100

    Fixed accordion styles

--- src/components/AddShadowButton.vue
+++ src/components/AddShadowButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="w-full border-8 border-white bg-black p-4 text-xl font-bold uppercase text-white transition-all hover:bg-white hover:text-black"
+    class="mt-4 w-full border-8 border-white bg-black p-4 text-xl font-bold uppercase text-white transition-all hover:bg-white hover:text-black"
     @click="store.addShadow"
   >
     Add Shadow
--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,18 +1,32 @@
 <template>
   <div
+    class="collapse collapse-arrow mt-4 rounded-none bg-secondary-background text-secondary-text"
     v-for="(shadow, index) in shadowStore.shadowArray"
     :key="index"
-    class="mb-6 mt-6 flex flex-col gap-4 bg-secondary-background p-6 text-secondary-text"
   >
-    <div class="">
-      <div class="relative">
-        <label for="range" class="mb-8 block text-center text-xl font-medium"
-          >Horizontal Offset</label
-        >
-        <TextInput
+    <input type="checkbox" />
+    <div class="collapse-title text-xl font-medium">Shadow {{ index + 1 }}</div>
+    <div class="collapse-content">
+      <div class="mt-4">
+        <div class="relative">
+          <label for="range" class="mb-8 block text-center text-xl font-medium"
+            >Horizontal Offset</label
+          >
+          <TextInput
+            label="Horizontal Offset"
+            class="absolute right-0 top-0 text-primary-text"
+            :storeValue="+shadow.horizontalOffset"
+            @update-horizontal-offset="
+              (value) => {
+                shadow.horizontalOffset = value;
+              }
+            "
+            :min="-100"
+            :max="100"
+          />
+        </div>
+        <Slider
           label="Horizontal Offset"
-          class="absolute right-0 top-0 text-primary-text"
-          :storeValue="+shadow.horizontalOffset"
           @update-horizontal-offset="
             (value) => {
               shadow.horizontalOffset = value;
@@ -20,29 +34,29 @@
           "
           :min="-100"
           :max="100"
+          :storeValue="+shadow.horizontalOffset"
         />
       </div>
-      <Slider
-        label="Horizontal Offset"
-        @update-horizontal-offset="
-          (value) => {
-            shadow.horizontalOffset = value;
-          }
-        "
-        :min="-100"
-        :max="100"
-        :storeValue="+shadow.horizontalOffset"
-      />
-    </div>
-    <div>
-      <div class="relative">
-        <label for="range" class="mb-8 block text-center text-xl font-medium"
-          >Vertical Offset</label
-        >
-        <TextInput
+      <div>
+        <div class="relative">
+          <label for="range" class="mb-8 block text-center text-xl font-medium"
+            >Vertical Offset</label
+          >
+          <TextInput
+            label="Vertical Offset"
+            class="absolute right-0 top-0 text-primary-text"
+            :storeValue="+shadow.verticalOffset"
+            @update-vertical-offset="
+              (value) => {
+                shadow.verticalOffset = value;
+              }
+            "
+            :min="-100"
+            :max="100"
+          />
+        </div>
+        <Slider
           label="Vertical Offset"
-          class="absolute right-0 top-0 text-primary-text"
-          :storeValue="+shadow.verticalOffset"
           @update-vertical-offset="
             (value) => {
               shadow.verticalOffset = value;
@@ -50,30 +64,30 @@
           "
           :min="-100"
           :max="100"
+          :storeValue="+shadow.verticalOffset"
         />
       </div>
-      <Slider
-        label="Vertical Offset"
-        @update-vertical-offset="
-          (value) => {
-            shadow.verticalOffset = value;
-          }
-        "
-        :min="-100"
-        :max="100"
-        :storeValue="+shadow.verticalOffset"
-      />
-    </div>
 
-    <div>
-      <div class="relative">
-        <label for="range" class="mb-8 block text-center text-xl font-medium"
-          >Blur Radius</label
-        >
-        <TextInput
+      <div>
+        <div class="relative">
+          <label for="range" class="mb-8 block text-center text-xl font-medium"
+            >Blur Radius</label
+          >
+          <TextInput
+            label="Blur Radius"
+            class="absolute right-0 top-0 text-primary-text"
+            :storeValue="+shadow.blurRadius"
+            @update-blur-radius="
+              (value) => {
+                shadow.blurRadius = value;
+              }
+            "
+            :min="0"
+            :max="100"
+          />
+        </div>
+        <Slider
           label="Blur Radius"
-          class="absolute right-0 top-0 text-primary-text"
-          :storeValue="+shadow.blurRadius"
           @update-blur-radius="
             (value) => {
               shadow.blurRadius = value;
@@ -81,30 +95,30 @@
           "
           :min="0"
           :max="100"
+          :storeValue="+shadow.blurRadius"
         />
       </div>
-      <Slider
-        label="Blur Radius"
-        @update-blur-radius="
-          (value) => {
-            shadow.blurRadius = value;
-          }
-        "
-        :min="0"
-        :max="100"
-        :storeValue="+shadow.blurRadius"
-      />
-    </div>
 
-    <div>
-      <div class="relative">
-        <label for="range" class="mb-8 block text-center text-xl font-medium"
-          >Spread</label
-        >
-        <TextInput
+      <div>
+        <div class="relative">
+          <label for="range" class="mb-8 block text-center text-xl font-medium"
+            >Spread</label
+          >
+          <TextInput
+            label="Spread"
+            class="absolute right-0 top-0 text-primary-text"
+            :storeValue="+shadow.spread"
+            @update-spread="
+              (value) => {
+                shadow.spread = value;
+              }
+            "
+            :min="-100"
+            :max="100"
+          />
+        </div>
+        <Slider
           label="Spread"
-          class="absolute right-0 top-0 text-primary-text"
-          :storeValue="+shadow.spread"
           @update-spread="
             (value) => {
               shadow.spread = value;
@@ -112,49 +126,45 @@
           "
           :min="-100"
           :max="100"
+          :storeValue="+shadow.spread"
         />
       </div>
-      <Slider
-        label="Spread"
-        @update-spread="
-          (value) => {
-            shadow.spread = value;
-          }
-        "
-        :min="-100"
-        :max="100"
-        :storeValue="+shadow.spread"
-      />
-    </div>
 
-    <div class="relative flex flex-col justify-center">
-      <label for="range" class="mb-8 block text-center text-xl font-medium"
-        >Inset</label
-      >
-      <BinaryToggle
-        @update-inset="
-          (value) => {
-            console.log(value);
-            shadow.inset = value;
-          }
-        "
-      />
-    </div>
-    <div class="relative flex flex-col justify-center">
-      <label for="range" class="mb-8 block text-center text-xl font-medium"
-        >Shadow Colour</label
-      >
-      <ColourPicker
-        :initial-colour="shadow.colour"
-        @update-colour="
-          (value) => {
-            console.log(value);
-            shadow.colour = value;
-          }
-        "
-      />
+      <div class="relative flex flex-col justify-center">
+        <label for="range" class="mb-8 block text-center text-xl font-medium"
+          >Inset</label
+        >
+        <BinaryToggle
+          @update-inset="
+            (value) => {
+              console.log(value);
+              shadow.inset = value;
+            }
+          "
+        />
+      </div>
+      <div class="relative mt-6 flex flex-col justify-center">
+        <label for="range" class="mb-8 block text-center text-xl font-medium"
+          >Shadow Colour</label
+        >
+        <ColourPicker
+          :initial-colour="shadow.colour"
+          @update-colour="
+            (value) => {
+              console.log(value);
+              shadow.colour = value;
+            }
+          "
+        />
+      </div>
     </div>
   </div>
+
+  <!-- <div
+    v-for="(shadow, index) in shadowStore.shadowArray"
+    :key="index"
+    class="mb-6 mt-6 flex flex-col gap-4 bg-secondary-background p-6 text-secondary-text"
+  ></div> -->
   <AddShadowButton />
 </template>
 
@@ -168,4 +178,4 @@ import { useShadowStore } from "../stores/ShadowStore";
 const shadowStore = useShadowStore();
 </script>
 
-<style scoped></style>
+<style></style>
