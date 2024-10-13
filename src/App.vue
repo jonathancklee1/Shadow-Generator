967b312278d8aa173256f4eb3d15df309798bdcc
Jonathan Lee <jonathancklee@gmail.com>
2024-10-13 19:55:06 +1100

    Adjusted layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="relative h-screen min-h-screen w-full bg-primary-background px-4 py-8"
+    class="relative h-screen w-full overflow-auto bg-primary-background px-4 py-8"
   >
     <AppHeader />
     <main
-      class="mx-auto mt-8 w-full gap-4 bg-primary-background px-3 pb-4 pt-4 lg:flex lg:max-h-[1200px] lg:overflow-y-auto lg:bg-transparent lg:px-0 lg:pt-0 xl:max-w-screen-xl"
+      class="mx-auto mt-8 w-full gap-4 bg-primary-background px-3 pb-4 pt-4 lg:flex lg:max-h-[1200px] lg:bg-transparent lg:px-0 lg:pt-0 xl:max-w-screen-xl"
     >
       <div class="basis-2/3">
         <CodeBlock />
--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,32 +1,45 @@
 <template>
-  <div class="max-h-full grow overflow-y-auto">
-    <div
-      class="collapse collapse-arrow mt-4 rounded-none bg-secondary-background text-secondary-text lg:mt-2"
-      v-for="(shadow, index) in shadowStore.shadowArray"
-      :key="index"
-    >
-      <input type="checkbox" />
+  <div>
+    <AddShadowButton />
+    <div class="mt-2 max-h-full grow overflow-y-auto">
       <div
-        class="collapse-title flex max-h-fit w-full items-center gap-4 text-xl font-medium"
+        class="collapse collapse-arrow mt-4 rounded-none bg-secondary-background text-secondary-text lg:mt-2"
+        v-for="(shadow, index) in shadowStore.shadowArray"
+        :key="index"
       >
-        <p
-          class="w-fit bg-primary-background p-4 text-xl font-medium text-primary-text"
-          v-html="shadowStore.getIndexShadow(index)"
-        ></p>
-      </div>
+        <input type="checkbox" />
+        <div
+          class="collapse-title flex max-h-fit w-full items-center gap-4 text-xl font-medium"
+        >
+          <p
+            class="w-fit bg-primary-background p-4 text-xl font-medium text-primary-text"
+            v-html="shadowStore.getIndexShadow(index)"
+          ></p>
+        </div>
 
-      <div class="collapse-content">
-        <div class="mt-8">
-          <div class="relative flex justify-between">
-            <label
-              for="range"
-              class="mb-8 block text-base font-medium uppercase tracking-widest"
-              >Horizontal Offset</label
-            >
-            <TextInput
+        <div class="collapse-content">
+          <div class="mt-8">
+            <div class="relative flex justify-between">
+              <label
+                for="range"
+                class="mb-8 block text-base font-medium uppercase tracking-widest"
+                >Horizontal Offset</label
+              >
+              <TextInput
+                label="Horizontal Offset"
+                class="rounded-none border-2 border-white text-primary-text"
+                :storeValue="+shadow.horizontalOffset"
+                @update-horizontal-offset="
+                  (value) => {
+                    shadow.horizontalOffset = value;
+                  }
+                "
+                :min="minValue"
+                :max="maxValue"
+              />
+            </div>
+            <Slider
               label="Horizontal Offset"
-              class="rounded-none border-2 border-white text-primary-text"
-              :storeValue="+shadow.horizontalOffset"
               @update-horizontal-offset="
                 (value) => {
                   shadow.horizontalOffset = value;
@@ -34,31 +47,31 @@
               "
               :min="minValue"
               :max="maxValue"
+              :storeValue="+shadow.horizontalOffset"
             />
           </div>
-          <Slider
-            label="Horizontal Offset"
-            @update-horizontal-offset="
-              (value) => {
-                shadow.horizontalOffset = value;
-              }
-            "
-            :min="minValue"
-            :max="maxValue"
-            :storeValue="+shadow.horizontalOffset"
-          />
-        </div>
-        <div class="mt-8">
-          <div class="relative flex justify-between">
-            <label
-              for="range"
-              class="mb-8 block text-base font-medium uppercase tracking-widest"
-              >Vertical Offset</label
-            >
-            <TextInput
+          <div class="mt-8">
+            <div class="relative flex justify-between">
+              <label
+                for="range"
+                class="mb-8 block text-base font-medium uppercase tracking-widest"
+                >Vertical Offset</label
+              >
+              <TextInput
+                label="Vertical Offset"
+                class="rounded-none border-2 border-white text-primary-text"
+                :storeValue="+shadow.verticalOffset"
+                @update-vertical-offset="
+                  (value) => {
+                    shadow.verticalOffset = value;
+                  }
+                "
+                :min="minValue"
+                :max="maxValue"
+              />
+            </div>
+            <Slider
               label="Vertical Offset"
-              class="rounded-none border-2 border-white text-primary-text"
-              :storeValue="+shadow.verticalOffset"
               @update-vertical-offset="
                 (value) => {
                   shadow.verticalOffset = value;
@@ -66,32 +79,32 @@
               "
               :min="minValue"
               :max="maxValue"
+              :storeValue="+shadow.verticalOffset"
             />
           </div>
-          <Slider
-            label="Vertical Offset"
-            @update-vertical-offset="
-              (value) => {
-                shadow.verticalOffset = value;
-              }
-            "
-            :min="minValue"
-            :max="maxValue"
-            :storeValue="+shadow.verticalOffset"
-          />
-        </div>
 
-        <div class="mt-8">
-          <div class="relative flex justify-between">
-            <label
-              for="range"
-              class="mb-8 block text-base font-medium uppercase tracking-widest"
-              >Blur Radius</label
-            >
-            <TextInput
+          <div class="mt-8">
+            <div class="relative flex justify-between">
+              <label
+                for="range"
+                class="mb-8 block text-base font-medium uppercase tracking-widest"
+                >Blur Radius</label
+              >
+              <TextInput
+                label="Blur Radius"
+                class="rounded-none border-2 border-white text-primary-text"
+                :storeValue="+shadow.blurRadius"
+                @update-blur-radius="
+                  (value) => {
+                    shadow.blurRadius = value;
+                  }
+                "
+                :min="0"
+                :max="maxValue"
+              />
+            </div>
+            <Slider
               label="Blur Radius"
-              class="rounded-none border-2 border-white text-primary-text"
-              :storeValue="+shadow.blurRadius"
               @update-blur-radius="
                 (value) => {
                   shadow.blurRadius = value;
@@ -99,32 +112,32 @@
               "
               :min="0"
               :max="maxValue"
+              :storeValue="+shadow.blurRadius"
             />
           </div>
-          <Slider
-            label="Blur Radius"
-            @update-blur-radius="
-              (value) => {
-                shadow.blurRadius = value;
-              }
-            "
-            :min="0"
-            :max="maxValue"
-            :storeValue="+shadow.blurRadius"
-          />
-        </div>
 
-        <div class="mt-8">
-          <div class="relative flex justify-between">
-            <label
-              for="range"
-              class="mb-8 text-base font-medium uppercase tracking-widest"
-              >Spread</label
-            >
-            <TextInput
+          <div class="mt-8">
+            <div class="relative flex justify-between">
+              <label
+                for="range"
+                class="mb-8 text-base font-medium uppercase tracking-widest"
+                >Spread</label
+              >
+              <TextInput
+                label="Spread"
+                class="rounded-none border-2 border-white text-primary-text"
+                :storeValue="+shadow.spread"
+                @update-spread="
+                  (value) => {
+                    shadow.spread = value;
+                  }
+                "
+                :min="minValue"
+                :max="maxValue"
+              />
+            </div>
+            <Slider
               label="Spread"
-              class="rounded-none border-2 border-white text-primary-text"
-              :storeValue="+shadow.spread"
               @update-spread="
                 (value) => {
                   shadow.spread = value;
@@ -132,61 +145,50 @@
               "
               :min="minValue"
               :max="maxValue"
+              :storeValue="+shadow.spread"
             />
           </div>
-          <Slider
-            label="Spread"
-            @update-spread="
-              (value) => {
-                shadow.spread = value;
-              }
-            "
-            :min="minValue"
-            :max="maxValue"
-            :storeValue="+shadow.spread"
-          />
-        </div>
 
-        <div class="relative flex flex-col justify-center">
-          <label
-            for="range"
-            class="mb-4 text-base font-medium uppercase tracking-widest"
-            >Inset</label
+          <div class="relative flex flex-col justify-center">
+            <label
+              for="range"
+              class="mb-4 text-base font-medium uppercase tracking-widest"
+              >Inset</label
+            >
+            <BinaryToggle
+              @update-inset="
+                (value) => {
+                  console.log(value);
+                  shadow.inset = value;
+                }
+              "
+            />
+          </div>
+          <div class="relative mt-6 flex flex-col justify-center">
+            <label
+              for="range"
+              class="mb-4 text-base font-medium uppercase tracking-widest"
+              >Shadow Colour</label
+            >
+            <ColourPicker
+              :initial-colour="shadow.colour"
+              @update-colour="
+                (value) => {
+                  console.log(value);
+                  shadow.colour = value;
+                }
+              "
+            />
+          </div>
+          <button
+            class="box-button mx-auto mt-8 w-full p-4 text-sm font-medium uppercase"
+            @click="() => shadowStore.removeShadow(index)"
           >
-          <BinaryToggle
-            @update-inset="
-              (value) => {
-                console.log(value);
-                shadow.inset = value;
-              }
-            "
-          />
+            - Remove Shadow
+          </button>
         </div>
-        <div class="relative mt-6 flex flex-col justify-center">
-          <label
-            for="range"
-            class="mb-4 text-base font-medium uppercase tracking-widest"
-            >Shadow Colour</label
-          >
-          <ColourPicker
-            :initial-colour="shadow.colour"
-            @update-colour="
-              (value) => {
-                console.log(value);
-                shadow.colour = value;
-              }
-            "
-          />
-        </div>
-        <button
-          class="box-button mx-auto mt-8 w-full p-4 text-sm font-medium uppercase"
-          @click="() => shadowStore.removeShadow(index)"
-        >
-          - Remove Shadow
-        </button>
       </div>
     </div>
-    <AddShadowButton />
   </div>
 </template>
 
